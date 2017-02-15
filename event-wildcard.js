cf00e5d452af21aad82c714eba52f32062e4ecc2
Joe Grund <joe.grund@intel.com>
2017-02-15 11:37:17 -0500

    HYD-7129 Remove node-clone
    Remove node-clone which is on the SDL blacklist.
    
    We should be able to replace with es6 destructuring + obj.clone
    
      - Remove node-clone.
    
    Signed-off-by: Joe Grund <joe.grund@intel.com>

--- event-wildcard.js
+++ event-wildcard.js
@@ -5,29 +5,28 @@
 
 'use strict';
 
-
 var emit = require('events').EventEmitter.prototype.emit;
-var clone = require('clone');
+var obj = require('intel-obj');
 
-module.exports = function eventWildcard (socket, next) {
-  if (socket.onevent !== onEvent)
-    socket.onevent = onEvent;
+module.exports = function eventWildcard(socket, next) {
+  if (socket.onevent !== onEvent) socket.onevent = onEvent;
 
   return next();
 };
 
-function onEvent (packet) {
+function onEvent(packet) {
   var args = packet.data || [];
 
-  if (packet.id != null)
-    args.push(this.ack(packet.id));
+  if (packet.id != null) args.push(this.ack(packet.id));
 
   emit.apply(this, args);
 
-  var wildcardArgs = clone(args);
+  var wildcardArgs = [...args];
+
   var eventName = wildcardArgs.splice(0, 1, '*');
 
-  wildcardArgs[1] = wildcardArgs[1] || {};
+  wildcardArgs[1] = obj.clone(wildcardArgs[1] || {});
+
   wildcardArgs[1].eventName = eventName[0];
 
   emit.apply(this, wildcardArgs);
