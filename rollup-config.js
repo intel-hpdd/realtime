0e298e8ad97ad9e189ae1ceff127358335813973
Joe Grund <joe.grund@intel.com>
2017-06-02 07:36:05 -0500

    Update configs.
    Signed-off-by: Joe Grund <joe.grund@intel.com>

--- rollup-config.js
+++ rollup-config.js
@@ -3,14 +3,42 @@ import bundleSize from 'rollup-plugin-bundle-size';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
+import replace from 'rollup-plugin-re';
 
 const { FABLE_SERVER_PORT: port = 61225 } = process.env;
 
 export default {
   entry: './Base.fsproj',
   dest: './dist/bundle.js',
-  external: ['socket.io'],
+  external: [
+    'socket.io',
+    'pg-native',
+    'url',
+    'tls',
+    'dns',
+    'events',
+    'util',
+    'crypto',
+    'net',
+    'assert',
+    'path',
+    'fs',
+    'stream',
+    'https',
+    'string_decoder',
+    'querystring'
+  ],
+  format: 'cjs',
   plugins: [
+    replace({
+      include: 'node_modules/pg/lib/index.js',
+      patterns: [
+        {
+          test: "require('./native')",
+          replace: '{}'
+        }
+      ]
+    }),
     babel({
       include: 'node_modules/@mfl/**/*',
       plugins: ['transform-object-rest-spread'],
@@ -18,9 +46,14 @@ export default {
     }),
     resolve({
       jsnext: true,
-      main: true
+      main: true,
+      browser: false
     }),
-    commonjs(),
+    commonjs({
+      namedExports: {
+        'node_modules/pg/lib/index.js': ['Pool', 'Client']
+      }
+    }),
     fable({
       babel: {
         presets: [['env', { targets: { node: 'current' }, modules: false }]],
@@ -30,6 +63,5 @@ export default {
       port
     }),
     bundleSize()
-  ],
-  format: 'cjs'
+  ]
 };
